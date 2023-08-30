e03dff4aaf7c2573f37001b132afa9531f094f37
Darrin Daugherty <darrin@darrin.pro>
2023-08-30 10:16:24 -0400

    Adding conditional practice

---
+++ main.js
@@ -0,0 +1,66 @@
+//Math Stuff
+
+/*
+
+if(1+1 > 5){
+    console.log("True Fact!")
+} else {
+    console.log("Lies!")
+}
+
+if(4+5 < 20){
+    console.log("True Fact!")
+} else {
+    console.log("Lies!")
+}
+
+if(6-2 >= 0){
+    console.log("True Fact!")
+} else {
+    console.log("Lies!")
+}
+
+
+
+//Dog People vs Cat People
+
+let LikesDogs = false
+
+if(LikesDogs === true){
+    console.log("You are a dog person!")
+}   else {console.log("You are a cat person!")
+}
+
+
+
+//Greetings in other languages
+
+let userName = "Darrin"
+let preferredLanguage = "German"
+
+if(preferredLanguage === "English"){
+    console.log(`Hello, ${userName}!`)
+} if(preferredLanguage === "Spanish"){
+    console.log(`Hola, ${userName}!`)
+} if(preferredLanguage === "French"){
+    console.log(`Bonjour, ${userName}!`)
+} else {
+    console.log(`Please select a valid language, ${userName}.`)
+}
+
+
+*/
+
+let grade = 92
+
+if(grade > 0 && grade < 69){
+    console.log("You got an F.")
+} if(grade >= 70 && grade < 76){
+    console.log("You got a D.")
+} if(grade > 76 && grade < 84){
+    console.log("You got a C. ")
+} if(grade >= 84 && grade < 92){
+    console.log("You got an B.")
+} if(grade >= 92 && grade < 100){
+    console.log("You got an A.")
+}
